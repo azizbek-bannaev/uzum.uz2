633f71e035915e253d68ac26d35df097908b49d3
azizbek-bannaev <bannaevazizbek2@gmail.com>
2024-01-16 21:02:39 +0500

    uzum market

--- src/router/wishis/Wishis.js
+++ src/router/wishis/Wishis.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Products from '../../components/products/Products';
+import Empty from '../../components/wishesempty/Empty';
+
+
+
 
 function Wishis() {
-  const wishes = useSelector(state => state.wishes.value)
+  const wishes = useSelector(s => s.wishes.value)
   console.log(wishes);
   return (
     <div>
-      <Products data={wishes} title="Sevimlilar"/>
+      {
+        wishes.length ? <Products title="Sevimlilar"/> : <Empty/>
+      }
+      <Products data={wishes}   />
     </div>
   )
 }
