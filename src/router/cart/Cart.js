65ae1b439d4bc9e21f47b212844d54956f4e8a68
azizbek-bannaev <bannaevazizbek2@gmail.com>
2024-01-17 16:53:24 +0500

    uzum market

--- src/router/cart/Cart.js
+++ src/router/cart/Cart.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import Products from '../../components/products/Products';
+
 import CartEmpty from '../../components/wishesempty/CartEmpty';
 function Cart() {
-  const wishes = useSelector(s => s.wishes.value)
+ 
   return (
     <div>
-      {
-        wishes.length ? <Products title="Sevimlilar"/> : <CartEmpty/>
-      }
-      <Products data={wishes}   />
+      
+       <CartEmpty/>
     </div>
   )
 }
